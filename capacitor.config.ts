f6a9e052739c8a849931a4a8deb062ccdd1f77c8
mariamoradev <mariainfo150704@gmail.com>
2024-08-12 11:16:22 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taller_correcto_movile',
+  webDir: 'www'
+};
+
+export default config;
