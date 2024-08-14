08b9a4b0d3d6292b48045488643cd32ec1ded61a
mariamoradev <mariainfo150704@gmail.com>
2024-08-14 00:21:59 -0500

    taller_movile_maria_mora

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-card',
@@ -8,8 +9,37 @@ import { Component, Input } from '@angular/core';
 export class CardComponent {
 
   @Input() array:any;
-  constructor() { }
+  constructor(private alertController: AlertController) { }
+
+  onCheckboxChange(event: any) {
+    const isChecked = event.detail.checked;
+    console.log('Checkbox state changed. Is checked:', isChecked);
+    // Aquí puedes realizar cualquier acción que desees según el estado del checkbox
+    this.presentAlert();
+  }
+
 
   ngOnInit() {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Excellent',
+      subHeader: '',
+      message: '¡Good job!.',
+      buttons: [
+        
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Okay clicked');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
 }
+
+
+
